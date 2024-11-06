5eda94db326e73cb95636448886a7206246f2176
Anurag Gupta <anurag.gupta108@gmail.com>
2024-11-06 17:29:45 +0530

    Revert "Complete API to accept or reject a trader invite"
    This reverts commit 2e4b532c29027a5adb30cc82bc1e5b0bfafb4b24.

--- backend/encryption_utils.ts
+++ backend/encryption_utils.ts
@@ -1,9 +1,5 @@
 import bcrypt from "bcrypt";
-import crypto from 'crypto'
-
 const saltRounds = 10;
-const iv = "I02y9ZV0nv1h80h91EXJVg==";
-const invitationCipherPrivateKey = "/PzEvpLIONi54y3B73qkyBZhJtcOSRsQ6LZm1P+wUkY=";
 
 export async function hashPassword(plaintextPassword: string) {
     return await bcrypt.hash(plaintextPassword, saltRounds);
@@ -11,16 +7,4 @@ export async function hashPassword(plaintextPassword: string) {
 
 export async function doPasswordsMatch(plaintextPassword: string, hashedPassword: string) {
     return await bcrypt.compare(plaintextPassword, hashedPassword);
-}
-
-export function encryptInvitationInfo(invitationId: string) {
-    let cipher = crypto.createCipheriv("aes-256-gcm", Buffer.from(invitationCipherPrivateKey, "base64"), Buffer.from(iv, "base64"));
-    let invitationCipher = cipher.update(invitationId, "utf8", "base64");
-    invitationCipher += cipher.final("base64");
-    return invitationCipher;
-}
-
-export function decryptInvitationInfo(invitationCipher: string) {
-    const decipher = crypto.createDecipheriv("aes-256-gcm", Buffer.from(invitationCipherPrivateKey, "base64"), Buffer.from(iv, "base64"));
-    return decipher.update(invitationCipher, "base64", "utf8");
 }
